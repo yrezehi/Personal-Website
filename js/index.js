6c06911cea3fca277c3caf7d986a4dc121c97250
unknown <yrezehi@gmail.com>
2023-07-16 10:52:00 -0700

    fix buildcommandprefix

--- js/index.js
+++ js/index.js
@@ -4,7 +4,9 @@ const commandPrefix = "Visitor@YREZEHI:/index$";
 const DIRECTORY_COLOR = "#5a73b3";
 const USER_COLOR = "#89a764";
 
-var terminalCursor = document.getElementById("terminal-cursor");
+const TERMINAL_CURSOR = "terminal-cursor";
+const TERMINAL_CARET = "terminal-caret";
+
 var terminalWindow = document.getElementById("terminal-window");
 
 const commands = {
@@ -25,18 +27,20 @@ function paddedText(text, value){
     return text.padEnd(value);
 }
 
-terminalCursor.onkeydown = function (event) {
+document.getElementById(TERMINAL_CARET).onkeydown = function (event) {
     if (event.key === 'Enter'){
-        parseCommand(terminalCursor.textContent);
+        parseCommand(document.getElementById(TERMINAL_CARET).textContent);
         event.preventDefault();
     }
 }
 
 document.body.onclick = function (event) {
-    terminalCursor.focus();
+    document.getElementById(TERMINAL_CARET).focus();
 }
 
 function buildCommandPrefix(){
+    moveCaretToNewLine();
+
     const userElement = document.createElement("span");
     userElement.classList.add(`text-[${USER_COLOR}]`);
     userElement.textContent = "Visitor@YREZEHI:";
@@ -47,21 +51,27 @@ function buildCommandPrefix(){
     directoryElement.textContent = "/";
     terminalWindow.appendChild(directoryElement);
 
-    const cursorElement = document.createElement("span");
-    cursorElement.id = "terminal-cursor";
-    cursorElement.spellcheck = false;
-    cursorElement.contentEditable = false;
-    cursorElement.classList.add("min-w-fit", "w-2", "caret-transparent", "align-middle", "color-transparent", "text-white", "outline-0");
-    terminalWindow.appendChild(cursorElement);
-
     const suffixElement = document.createElement("span");
     suffixElement.textContent = "$";
     suffixElement.classList.add("text-white");
     terminalWindow.appendChild(suffixElement);
+
+    const caretElement = document.createElement("span");
+    caretElement.id = TERMINAL_CARET;
+    caretElement.spellcheck = false;
+    caretElement.contentEditable = true;
+    caretElement.classList.add("min-w-fit", "w-2", "caret-transparent", "align-middle", "color-transparent", "text-white", "outline-0");
+    terminalWindow.appendChild(caretElement);
+
+    const cursorElement = document.createElement("span");
+    cursorElement.id = TERMINAL_CURSOR;
+    cursorElement.classList.add("animate-pulse", "bg-white", "w-2", "h-4", "mt-1");
+    terminalWindow.appendChild(cursorElement);
 }
 
 function moveCaretToNewLine(){
-    terminalCursor.contentEditable = false;
+    document.getElementById(TERMINAL_CARET).contentEditable = false;
+    document.getElementById(TERMINAL_CURSOR).remove();
 }
 
 function parseCommand(command){
