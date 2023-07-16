baf6e4fef026406e208ea9f8614381e81233cd1b
unknown <yrezehi@gmail.com>
2023-07-16 09:35:34 -0700

    focus cursor

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
+
+const commandPrefix = "Visitor@YREZEHI:/index$";
+
 var terminalCursor = document.getElementById("terminal-cursor");
 
-document.getElementById("terminal-cursor").onkeydown = function (event) {
+terminalCursor.onkeydown = function (event) {
     if (event.key === 'Enter'){
         event.preventDefault();
     }
+}
+
+document.body.onclick = function (event) {
+    terminalCursor.focus();
 }
