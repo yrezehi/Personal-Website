7d3a97844753e07a61304ca0f26e291dcc657def
unknown <yrezehi@gmail.com>
2023-07-16 10:36:39 -0700

    build command prefix

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ const DIRECTORY_COLOR = "#5a73b3";
 const USER_COLOR = "#89a764";
 
 var terminalCursor = document.getElementById("terminal-cursor");
+var terminalWindow = document.getElementById("terminal-window");
 
 const commands = {
     "help": {
@@ -37,24 +38,26 @@ document.body.onclick = function (event) {
 
 function buildCommandPrefix(){
     const userElement = document.createElement("span");
+    userElement.classList.add(`text-[${USER_COLOR}]`);
     userElement.textContent = "Visitor@YREZEHI:";
-    document.body.appendChild(userElement);
+    terminalWindow.appendChild(userElement);
+
+    const directoryElement = document.createElement("span");
+    directoryElement.classList.add(`text-[${DIRECTORY_COLOR}]`);
+    directoryElement.textContent = "/";
+    terminalWindow.appendChild(directoryElement);
 
     const cursorElement = document.createElement("span");
     cursorElement.id = "terminal-cursor";
     cursorElement.spellcheck = false;
     cursorElement.contentEditable = false;
-    cursorElement.classList.add("min-w-fit w-2 ml-1 caret-transparent align-middle color-transparent text-white outline-0");
-    document.body.appendChild(cursorElement);
-
-    const directoryElement = document.createElement("span");
-    directoryElement.classList.add(`text-[${DIRECTORY_COLOR}]`);
-    document.body.appendChild(cursorElement);
+    cursorElement.classList.add("min-w-fit", "w-2", "caret-transparent", "align-middle", "color-transparent", "text-white", "outline-0");
+    terminalWindow.appendChild(cursorElement);
 
     const suffixElement = document.createElement("span");
-    suffixElement.classList.add("text-white");
     suffixElement.textContent = "$";
-    document.body.appendChild(suffixElement);
+    suffixElement.classList.add("text-white");
+    terminalWindow.appendChild(suffixElement);
 }
 
 function moveCaretToNewLine(){
